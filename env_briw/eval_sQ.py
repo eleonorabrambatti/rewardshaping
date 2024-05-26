efe984a3078b57bf77a722e36470135dd413ec00
eleonorabrambatti <e.brambatti@campus.unimib.it>
2024-05-26 10:21:42 +0200

    update codice completo

---
+++ env_briw/eval_sQ.py
@@ -0,0 +1,111 @@
+import numpy as np
+import pandas as pd
+import os
+import pickle
+
+
+def evaluate_policy_and_log_detailed_metrics(env, sq_class, output_dir, n_eval_episodes=10):
+    subdir = 'pickle_file'
+    full_path = os.path.join(output_dir, subdir)
+    os.makedirs(full_path, exist_ok=True)
+    total_rewards = []
+    metrics = {
+        'demand': [],
+        'expired_items': [],
+        'stock': [],  # Will be updated to reflect last m elements sum
+        'in_transit': [],
+        'lost_sales': [],
+        'satisfied_demand': [],
+        'orders': [],
+        'average_reward': [],
+        'reward_std': [],
+    }
+    episodes = {key: [] for key in metrics}
+    for episode in range(n_eval_episodes):
+        obs = env.reset()
+        done = False
+        episode_rewards = 0
+        episode_metrics = {key: [] for key in metrics}
+
+        while not done:
+            action = sq_class.act(env.total_stock)
+            action = np.around(action).astype(int)
+            # if action != 3:
+            #    print(f'action: {action}')
+            obs, reward, done, info = env.step(action)
+            episode_rewards += reward
+
+            # Accumulate metrics for each step
+            for key in metrics:
+                if key == 'stock':
+                    # Directly use the 'stock' value from info which is now consistent
+                    episode_metrics[key].append(info[key])
+                else:
+                    episode_metrics[key].append(info.get(key, 0))
+        total_rewards.append(episode_rewards)
+
+        for key in metrics:
+            if episode == 0:
+                episodes[key] = episode_metrics[key]
+            else:
+                episodes[key] = [
+                    x+y for x, y in zip(episodes[key], episode_metrics.get(key, [0]*len(episodes[key])))]
+
+        # Calculate and aggregate episode metrics
+            metrics[key].append(np.mean(episode_metrics[key]))
+
+    # Calculate average metrics over all episodes
+    avg_metrics = {key: np.mean(value) for key, value in metrics.items()}
+    filename = os.path.join(full_path, f'avg_metrics.pkl')
+    with open(filename, 'wb') as f:
+        pickle.dump(avg_metrics, f)
+    avg_reward = np.mean(total_rewards)
+    filename = os.path.join(full_path, f'avg_reward.pkl')
+    with open(filename, 'wb') as f:
+                pickle.dump(avg_reward, f)
+    std_reward = np.std(total_rewards)
+    filename = os.path.join(full_path, f'std_reward.pkl')
+    with open(filename, 'wb') as f:
+                pickle.dump(std_reward, f)
+    # print(f"Average Reward: {avg_reward}, Reward STD: {std_reward}")
+    for key in episodes:
+        episodes[key] = [x/n_eval_episodes for x in episodes[key]]
+
+        if key != 'reward_std':
+            value=episodes[key]
+            filename = os.path.join(full_path, f'{key}.pkl')
+            with open(filename, 'wb') as f:
+                pickle.dump(value, f)
+
+
+
+def save_metrics_to_dataframe(output_dir, config_details):
+    subdir = 'pickle_file'
+    full_path = os.path.join(output_dir, subdir)
+    os.makedirs(full_path, exist_ok=True)
+
+    subdir_avg_metrics = 'pickle_file/avg_metrics.pkl'
+    subdir_avg_reward = 'pickle_file/avg_reward.pkl'
+    subdir_std_reward = 'pickle_file/std_reward.pkl'
+    avg_metrics_path = os.path.join(output_dir, subdir_avg_metrics)    
+    avg_reward_path = os.path.join(output_dir, subdir_avg_reward)
+    std_reward_path = os.path.join(output_dir, subdir_std_reward)
+
+    with open(avg_metrics_path, 'rb') as file:
+        metrics_dict = pickle.load(file)
+    with open(avg_reward_path, 'rb') as file:
+        avg_reward = pickle.load(file)
+    with open(std_reward_path, 'rb') as file:
+        std_reward = pickle.load(file)
+
+    metrics_dict['config_details'] = str(config_details)
+    metrics_dict['avg_reward'] = avg_reward
+    metrics_dict['std_reward'] = std_reward
+
+    filename='evaluation_metrics_sq.csv'
+    df = pd.DataFrame([metrics_dict])
+    filename = os.path.join(output_dir, filename)
+    if not os.path.isfile(filename):
+        df.to_csv(filename, index=False)
+    else:
+        df.to_csv(filename, mode='a', header=False, index=False)
--- env_briw/main.py
+++ env_briw/main.py
@@ -7,11 +7,13 @@ import train_sQ
 import train_dqn
 import eval_ppo
 import eval_BS
+import eval_sQ
 import eval_dqn
 import plot
 import BS
 import sQ
 import time
+import pickle
 from stable_baselines3 import PPO
 from stable_baselines3 import DQN
 import scipy.optimize as optimize
@@ -28,13 +30,13 @@ from scipy.optimize import Bounds
 
 
 
-ppo = False
+ppo = True
 dqn = False
-bs = True
+bs = False
 sq = False
 
 
-train = False
+train = True
 plot_train = True
 eval = False
 plot_eval = False
@@ -94,114 +96,170 @@ def main():
             os.makedirs(full_path, exist_ok=True)
 
             if train:
-                model = train_dqn.train_dqn_model(
-                    i, env, policy_kwargs, learning_rate, total_timesteps)
-                train_dqn.save_logs()
-                # timesteps, results = plot.load_results_and_timesteps(
-                #    'timesteps.pkl', 'results.pkl')
-                # plot.plot_reward_convergence(
-                #    timesteps, results, config_details)
-            else:
-                model = DQN('MlpPolicy', env, policy_kwargs=policy_kwargs, verbose=0,
-                            learning_rate=learning_rate, n_steps=32, batch_size=32,
-                            n_epochs=10, clip_range=0.1, gamma=0.99, ent_coef=0.01)
-                model = DQN.load(f"./logs/ppo_model_{i}")
+                model = train_dqn.train_dqn_model(env, learning_rate, total_timesteps, full_path, batch_size, buffer_size, gradient_steps, target_update_interval)
+            if plot_train:
+                train_dqn.save_logs(full_path)
+                plot.plot_reward_convergence(full_path)
+
+            if eval:
+                model_path = os.path.join(full_path, f"./logs/dqn_model")
+
+                model = DQN.load(model_path)
+                eval_dqn.evaluate_policy_and_log_detailed_metrics(model, env, full_path, n_eval_episodes=20)
+
+            if plot_eval:
+                eval_dqn.save_metrics_to_dataframe(full_path, config_details)
+                plot.plot_episodes_metrics(steps, full_path) 
 
         elif bs:
             subdir = 'bs'
             full_path = os.path.join(output_dir, subdir)
             os.makedirs(full_path, exist_ok=True)
 
-            bf_manual_path = os.path.join(full_path, 'brute_force_manual')
-            os.makedirs(bf_manual_path, exist_ok=True)
+            bs_path = os.path.join(full_path, 'powell')
+            #bs_path = os.path.join(full_path, 'brute_force_scipy')
+            #bs_path = os.path.join(full_path, 'brute_force_manual')
+            os.makedirs(bs_path, exist_ok=True)
             
             if train:
                 start_time = time.time()
 
-                # powell_path = os.path.join(full_path, 'powell')
-                # os.makedirs(powell_path, exist_ok=True)
-                #bnds = [(5, 25)]
-                # initial_guess = 5
-                # res = optimize.minimize(train_BS.fun, initial_guess, args=(
-                #    env,), method='Powell', bounds=bnds, tol=0.00001)
-                # end_time = time.time()
+                
+                bnds = [(5, 25)]
+                initial_guess = 5
+                res = optimize.minimize(train_BS.fun, initial_guess, args=(
+                    env,), method='Powell', bounds=bnds, tol=0.00001)
+                end_time = time.time()
 
-                # elapsed_time = end_time - start_time
-                # print(elapsed_time)
-                # print("Best s:", np.around(res.x[0]))
+                elapsed_time = end_time - start_time
+                print(elapsed_time)
+                print("res:", np.around(res.x))
+                print("Best s:", np.around(res.x[0]))
+                print("Best average reward:", -res.fun)
+                """
+                subdir = 'pickle_file_bs'
+                levels_path = os.path.join(subdir, f'levels.pkl')    
+                rewards_path = os.path.join(subdir, f'avg_rewards.pkl')
+                with open(levels_path, 'rb') as file:
+                    levels = pickle.load(file)
+                with open(rewards_path, 'rb') as file:
+                    rewards = pickle.load(file)
 
-                # print("Best average reward:", -res.fun)
+                full_path = os.path.join(bs_path, 'pickle_file')
+                os.makedirs(full_path, exist_ok=True)
+                filename = os.path.join(full_path, f'levels.pkl')
+                with open(filename, 'wb') as f:
+                    pickle.dump(levels, f)
+                filename = os.path.join(full_path, f'avg_rewards.pkl')
+                with open(filename, 'wb') as f:
+                        pickle.dump(rewards, f)"""
+                full_path = os.path.join(bs_path, 'pickle_file')
+                os.makedirs(full_path, exist_ok=True)
+                filename = os.path.join(full_path, f'best_base_stock.pkl')
+                with open(filename, 'wb') as f:
+                        pickle.dump(res.x[0], f)
 
-                # bf_scipy_path = os.path.join(full_path, 'brute_force_scipy')
-                # os.makedirs(bf_scipy_path, exist_ok=True)
-                #rranges = [slice(5, 15, 1)]
-                #resbrute = optimize.brute(train_BS.fun, rranges, args=(
-                #    env,), full_output=True, finish=None)
 
-                #end_time = time.time()
-                #elapsed_time = end_time - start_time
-                #print(elapsed_time)
-                #print(resbrute[0])
-                #print(resbrute[1])
-                #print(resbrute[3])
-                #grid_values = resbrute[3]
-                #print(grid_values.size)
 
-                bf_manual_path = os.path.join(full_path, 'brute_force_manual')
-                os.makedirs(bf_manual_path, exist_ok=True)
-                train_BS.train_bs_policy(
-                     env, bf_manual_path, 5, 25, total_timesteps)
+                """ rranges = [slice(5, 15, 1)]
+                resbrute = optimize.brute(train_BS.fun, rranges, args=(
+                    env,), full_output=True, finish=None)
+
                 end_time = time.time()
                 elapsed_time = end_time - start_time
                 print(elapsed_time)
-            if plot_train:    
-                plot.plot_rewards_per_bs_level(bf_manual_path)
+                print(resbrute[0])
+                print(resbrute[1])
+                print(resbrute[3])
+                grid_values = resbrute[3]
+                print(grid_values.size)
 
-                # df_configurations.at[i,
-                #                     'base_stock_level'] = base_stock_level
-                # df_configurations.to_excel(
-                #     excel_path, index=False, engine='openpyxl')
-            else:
-                base_stock_level = config['base_stock_level']
+                full_path = os.path.join(bs_path, 'pickle_file')
+                os.makedirs(full_path, exist_ok=True)
+                filename = os.path.join(full_path, f'levels.pkl')
+                with open(filename, 'wb') as f:
+                    pickle.dump(list(range(5, 15)), f)
+                filename = os.path.join(full_path, f'avg_rewards.pkl')
+                with open(filename, 'wb') as f:
+                        pickle.dump(-resbrute[3], f)
+                filename = os.path.join(full_path, f'best_base_stock.pkl')
+                with open(filename, 'wb') as f:
+                        pickle.dump(resbrute[0], f) """
+                
+
+                #train_BS.train_bs_policy(
+                #     env, bs_path, 5, 25, total_timesteps)
+                #end_time = time.time()
+                #elapsed_time = end_time - start_time
+                #print(elapsed_time)
+
+
+            if plot_train:
+                plot.plot_rewards_per_bs_level(bs_path)
+            
             if eval:
+                best_bs_path = os.path.join(bs_path, 'pickle_file/best_base_stock.pkl')
+                with open(best_bs_path, 'rb') as file:
+                    base_stock_level = pickle.load(file)
                 bs_class = BS.BSpolicy(base_stock_level)
-                avg_reward, std_reward, avg_metrics, episodes_metrics = eval_BS.evaluate_policy_and_log_detailed_metrics(
-                    env, bs_class, n_eval_episodes=20)
-                eval_BS.save_metrics_to_dataframe(
-                    avg_metrics, config_details, avg_reward, std_reward, filename='evaluation_metrics_bs.csv')
-                plot.plot_episodes_metrics(
-                    episodes_metrics, config_details, steps)
+                eval_BS.evaluate_policy_and_log_detailed_metrics(env, bs_class, bs_path, n_eval_episodes=20)
+            if plot_eval:
+                eval_BS.save_metrics_to_dataframe(bs_path, config_details)
+                plot.plot_episodes_metrics(steps, bs_path)
+
         elif sq:
+
+            subdir = 'sq'
+            full_path = os.path.join(output_dir, subdir)
+            os.makedirs(full_path, exist_ok=True)
+
+            #sq_path = os.path.join(full_path, 'powell')
+            #sq_path = os.path.join(full_path, 'brute_force_scipy')
+            sq_path = os.path.join(full_path, 'brute_force_manual')
+            os.makedirs(sq_path, exist_ok=True)
             if train:
 
-                # Definiamo i limiti per s e Q
-                # Cambia questi limiti in base al tuo problema
+                """ # Definiamo i limiti per s e Q
 
                 bnds = [(0, 10), (0, 10)]
 
                 # Eseguiamo l'ottimizzazione
                 # Cambia la stima iniziale in base al tuo problema
-                #initial_guess = (5, 5)
-                #res = optimize.minimize(train_sQ.fun, initial_guess, args=(
-                #    env,), method='Powell', bounds=bnds, tol=0.000001, options={'disp': True})
+                initial_guess = (5, 5)
+                res = optimize.minimize(train_sQ.fun, initial_guess, args=(
+                    env,), method='Powell', bounds=bnds, tol=0.000001, options={'disp': True})
 
-                #print("Best s:", np.around(res.x[0]))
-                #print("Best Q:", np.around(res.x[1]))
-                #print("Best average reward:", -res.fun)
-                # env.seed(42)
-                best_s, best_Q, levels, rewards = train_sQ.train_sQ_policy(
-                     env, 0, 10, 0, 10, total_timesteps)
-                plot.plot_rewards_per_sq_level(levels, rewards, config_details)
-            # else:
-            #    base_stock_level = config['base_stock_level']
-            # if eval:
-            #    bs_class = sQ.sQpolicy(base_stock_level)
-            #    avg_reward, std_reward, avg_metrics, episodes_metrics = eval_BS.evaluate_policy_and_log_detailed_metrics(
-            #        env, bs_class, n_eval_episodes=20)
-            #    eval_BS.save_metrics_to_dataframe(
-            #        avg_metrics, config_details, avg_reward, std_reward, filename='evaluation_metrics_bs.csv')
-            #    plot.plot_episodes_metrics(
-            #        episodes_metrics, config_details, steps)
+                print("Best s:", np.around(res.x[0]))
+                print("Best Q:", np.around(res.x[1]))
+                print("Best average reward:", -res.fun)
+
+                full_path = os.path.join(sq_path, 'pickle_file')
+                os.makedirs(full_path, exist_ok=True)
+                filename = os.path.join(full_path, f'best_s.pkl')
+                with open(filename, 'wb') as f:
+                        pickle.dump(res.x[0], f)
+                filename = os.path.join(full_path, f'best_Q.pkl')
+                with open(filename, 'wb') as f:
+                        pickle.dump(res.x[1], f) """
+
+                env.seed(42)
+                train_sQ.train_sQ_policy(env, 0, 10, 0, 10, sq_path, total_timesteps)
+
+            if plot_train:
+                plot.plot_rewards_per_sq_level(sq_path)
+            
+            if eval:
+                best_sq_path = os.path.join(sq_path, 'pickle_file')
+                with open(best_sq_path + '/best_s.pkl', 'rb') as file:
+                    best_s = pickle.load(file)
+                with open(best_sq_path + '/best_Q.pkl', 'rb') as file:
+                    best_Q = pickle.load(file)
+                sq_class = sQ.sQpolicy(best_s, best_Q)
+                eval_sQ.evaluate_policy_and_log_detailed_metrics(env, sq_class, sq_path, n_eval_episodes=20)
+            if plot_eval:
+                eval_sQ.save_metrics_to_dataframe(sq_path, config_details)
+                plot.plot_episodes_metrics(steps, sq_path)
+           
 main()
 
 
